4ae0fe317003d8e3e39fa2479f9af32d7d10866f
Carla Viviane <carla_dourado@estudante.sesisenai.org.br>
2026-05-11 16:51:28 -0300

    .

--- backend/src/time/entities/time.entity.ts
+++ backend/src/time/entities/time.entity.ts
@@ -13,8 +13,8 @@ export class Time {
 
   // Método para o Front-end saber o nível do time
   get rank(): string {
-    if (this.vitorias < 5) return 'Bronze';
-    if (this.vitorias <= 10) return 'Prata';
+    if (this.vitorias < 10) return 'Bronze';
+    if (this.vitorias <= 20) return 'Prata';
     return 'Ouro';
   }
 }
